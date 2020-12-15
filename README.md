c8f269375c733b4e50d9482e7612c0127db93373
Harikrishnan Kottappurath Unnikrishnan <ku.harikrishnan@gmail.com>
2020-12-14 20:17:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Resume Builder App
+
+A simple App to create Resumes using React.
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+      <h3>Resume Builder</h3>
+      <h4>Select your Resume Template</h4>
+      </main>
+    </div>
+  );
+}
+
+export default App;
